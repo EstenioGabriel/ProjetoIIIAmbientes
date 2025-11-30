7d33aa21f5b18cb6c9e0c779322f742f64467b53
Paulo Zek Alexandre Gomes <paulozekagomes@gmail.com>
2025-11-30 17:36:22 -0300

    Renamed p to port

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const path = require("path");
 const app = express();
 // Cria a instância da aplicação Express
 
-const p = 3000;
-// Define a porta (variável 'p') usada pelo servidor
+const port = 3000;
+// Define a porta (variável 'port') usada pelo servidor
 
 app.use(express.static(path.join(__dirname, "public")));
 // Essa função faz o Express servir todo o conteúdo da pasta public como conteúdo do FrontEnd
 
-app.listen(p, () => {
+app.listen(port, () => {
   // NÃO PODE SER ALTERADO – PARTE DA BIBLIOTECA
   // listen(PORTA DO SERVIDOR , callback) é obrigatório desse jeito.
 
-  console.log(`Server running on port ${p}`);
+  console.log(`Server running on port ${port}`);
 });
 
 /*
