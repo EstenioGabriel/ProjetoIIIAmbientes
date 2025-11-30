2f25204f624d592b2f6ada1b08d5613d39ed5d1b
Estenio Gabriel <esteniogabrieldelima@gmail.com>
2025-11-30 19:09:59 -0300

    Rename variable li to limitPerPage

--- public/app.js
+++ public/app.js
@@ -71,8 +71,8 @@ async function lbt() {
         let listPokemonTypes = await responsePokemonRote.json();
 
         let pokemonTypesList = [];
-        var li = listPokemonTypes.pokemon.length > 100 ? 100 : listPokemonTypes.pokemon.length; // Limita a 100
-        for(var i = 0; i < li; i++) {
+        let limitPerPage = listPokemonTypes.pokemon.length > 100 ? 100 : listPokemonTypes.pokemon.length; // Limita a 100
+        for(var i = 0; i < limitPerPage; i++) {
             pokemonTypesList.push(fetch(listPokemonTypes.pokemon[i].pokemon.url));
         }
 
